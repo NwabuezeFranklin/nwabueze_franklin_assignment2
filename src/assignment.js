ec249d73219bebfb47274d07ab729b8b8742afab
Nwabueze Franklin <99493414+NwabuezeFranklin@users.noreply.github.com>
2022-07-03 17:05:26 +0000

    array assignments

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,14 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let total = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    total += arrayOfNumbers[i];}
+
+  return total;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+    
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +33,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+      if (arrayOfNumbers[i] % 2 == 0) {
+        count++;
+   }
+    }
+  return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
